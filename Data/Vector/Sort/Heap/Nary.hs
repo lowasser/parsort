db1b2e87fece67a4850eafcf399118eaf2a8b9a7
Louis Eric Wasserman <lowasser@ursa.cs.uchicago.edu>
2011-04-18 15:32:53 -0500

    Added n-ary heap sort implementation

---
+++ Data/Vector/Sort/Heap/Nary.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE ImplicitParams #-}
+module Data.Vector.Sort.Heap.Nary (sortBy) where
+
+import Control.Monad
+
+import Data.Bits
+import qualified Data.Vector.Primitive as P
+import Data.Vector.Sort.Common
+
+arity :: Int
+arity = 3
+
+sortBy :: Vector v a => LEq a -> v a -> v a
+sortBy = sortPermM sortByM
+
+sortByM :: (?cmp :: Comparator) => PMVector s Elem -> ST s ()
+sortByM = sequentialSort $ \ xs -> do
+  heapify xs
+  sortHeap xs
+
+heapify :: (?cmp :: Comparator) => PMVector s Elem -> ST s ()
+heapify xs = go_heapify ((lengthM xs - 1) `quot` arity) where
+  go_heapify k = when (k >= 0) $ do
+    siftTop xs k =<< read xs k
+    go_heapify (k-1)
+
+siftTop :: (?cmp :: Comparator) => PMVector s Elem -> Int -> Elem -> ST s ()
+siftTop !xs k x
+  | arity * k + 1 < lengthM xs = do
+      minChild <- P.foldl1M' (\ i j -> do
+	  xi <- read xs i
+	  xj <- read xs j
+	  if xi <=? xj then return i else return j) (P.takeWhile (< lengthM xs) $ P.enumFromN (arity * k + 1) arity)
+      xC <- read xs minChild
+      if xC <=? x then write xs k x else write xs k xC >> siftTop xs xC x
+  | otherwise = write xs k x
+
+sortHeap :: (?cmp :: Comparator) => PMVector s Elem -> ST s ()
+sortHeap xs = go_sort (lengthM xs - 1) where
+  go_sort k = when (k > 0) $ do
+    top <- read xs 0
+    x <- read xs k
+    write xs k top
+    siftTop xs 0 x
+    go_sort (k-1)
