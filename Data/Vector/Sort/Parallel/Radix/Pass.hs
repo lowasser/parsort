d91740cb5f93c6fd516c63abf2a895df461949bc
Louis Eric Wasserman <lowasser@polaris.cs.uchicago.edu>
2011-04-22 14:47:14 -0500

    Rewrite parallel radix sort

--- Data/Vector/Sort/Parallel/Radix/Pass.hs
+++ Data/Vector/Sort/Parallel/Radix/Pass.hs
@@ -11,36 +11,48 @@ import Data.Vector.Sort.Radix.Class
 import Data.Vector.Sort.Radix.Utils
 import Data.Vector.Sort.Parallel.Stream
 
+import Data.Vector.Fusion.Stream (liftStream)
+import Data.Vector.Fusion.Stream.Monadic (zipWith, prescanl)
+
+import Data.Vector.Generic.Mutable (unsafeNew, transform)
+
 import qualified Data.Vector as V
 import qualified Data.Vector.Primitive as P
 
 import Prelude hiding (read)
 
-type RadixPass a = Int -> PMVector RealWorld a -> PMVector RealWorld a -> IO ()
-
 {-# INLINE radixPass #-}
-radixPass :: forall a . Radix a => RadixPass a
-radixPass !pass !arr !tmp = do
-  arrF <- P.unsafeFreeze arr
-  let !n = lengthM arr
+radixPass :: P.Prim a => (a -> Word8) -> PVector a -> IO (PVector a)
+radixPass look !arr = do
+  let !n = length arr
       !nCaps = numCapabilities
       !chunkSize = (n + nCaps - 1) `quot` nCaps
-      chunkAt i = let k = chunkSize `min` n - i in slice i k arrF
-      chunkCount i = P.create $ countUp $ P.map (radix pass) (chunkAt i)
+      chunkAt i = let k = chunkSize `min` n - i in slice i k arr
+      chunkCount i = P.create $ countUp $ P.map look $ chunkAt i
       !chunkCounts = parVector (V.map chunkCount $ V.enumFromStepN 0 chunkSize nCaps)
-      !cumCounts = V.scanl (P.zipWith (+)) (P.replicate 256 0) chunkCounts
-      totCounts = index cumCounts nCaps
-      !offsets = P.prescanl (+) 0 totCounts
+  tmp <- unsafeNew n
   lock <- newEmptyMVar
-  let go_move !i = do
-	!counter <- P.thaw (P.zipWith (+) offsets (index cumCounts i))
+  let go_move (i, counter) = forkOnIO i $ do
+	!counter <- P.thaw counter
 	let do_move x = do
 	      let !b = fromIntegral $ radix pass x
-	      i <- read counter b
-	      write counter b (i+1)
-	      write tmp i x
+	      j <- read counter b
+	      write counter b (j+1)
+	      write tmp j x
 	P.mapM_ do_move (chunkAt (i * chunkSize))
 	putMVar lock ()
-  P.mapM_ (\ i -> forkOnIO i $ go_move i) (P.enumFromN 0 nCaps)
+  P.mapM_ go_move $ P.imap (,) $ offsets chunkCounts
   replicateM_ nCaps (takeMVar lock)
-  copyM arr tmp
+  P.unsafeFreeze tmp
+
+{-# INLINE offsets #-}
+offsets :: VVector (PVector Int) -> Vector (PVector Int)
+offsets !chunkCounts = prescanl (P.zipWith (+)) (P.replicate 256 0) chunkCounts
+  where !tots = totalOffsets chunkCounts
+
+totalOffsets :: VVector (PVector Int) -> PVector Int
+totalOffsets !counts = P.create $ do
+  tot <- unsafeNew 256
+  V.forM_ counts $ \ count -> transform (zipWith (+) (liftStream $ stream count)) tot
+  transform (prescanl (+) 0) tot
+  return tot
